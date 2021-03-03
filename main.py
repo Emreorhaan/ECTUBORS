37e8c4f9121c3d0c75090916a19726d49118d38c
Emre Orhan <47822524+emre44orhan@users.noreply.github.com>
2021-03-03 17:42:38 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+import discord
+from discord.ext import commands
+
+token = ""
+
+class Bot(commands.Bot):
+	def __init__(self):
+		super().__init__(command_prefix = "e!", pm_help = None, )
+		
+		self.load_extension("commands.kur")
+		
+	async def on_ready(self):
+		print("Bot Açıldı")
+	
+bot = Bot()
+bot.run(token)
+
