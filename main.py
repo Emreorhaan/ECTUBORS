c05e3ac4920ab5aa4b125f3a040f96ca6d754b28
Emre Orhan <47822524+emre44orhan@users.noreply.github.com>
2021-03-03 17:46:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ class Bot(commands.Bot):
 	def __init__(self):
 		super().__init__(command_prefix = "e!", pm_help = None, )
 		
-		self.load_extension("commands.kur")
+		self.load_extension("kur")
 		
 	async def on_ready(self):
 		print("Bot Açıldı")
