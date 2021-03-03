4aa5cf5cea0fcb668ab97cb06d353a6ea765885d
Emre Orhan <47822524+emre44orhan@users.noreply.github.com>
2021-03-03 17:43:24 +0300

    Create cekemeyenler_utansin.py

---
+++ cekemeyenler_utansin.py
@@ -0,0 +1,65 @@
+import requests
+import json
+from bs4 import BeautifulSoup
+
+_borsa_url = "http://bigpara.hurriyet.com.tr/borsa/canli-borsa/"
+_doviz_url = "http://bigpara.hurriyet.com.tr/doviz/"
+_altin_url = "http://bigpara.hurriyet.com.tr/altin/"
+
+def kurcek():
+	global _doviz_verileri
+	global _altin_verileri
+	
+	_r2 = requests.get(_doviz_url)
+	_soup2 = BeautifulSoup(_r2.content,"html.parser")
+	
+	_r3 = requests.get(_altin_url)
+	_soup3 = BeautifulSoup(_r3.content,"html.parser")
+	
+	_doviz_verisi = _soup2.find_all("div",{"class":"tableBox srbstPysDvz"})
+	
+	_altin_verisi = _soup3.find_all("div",{"class":"table wide pgAltin"})
+	
+	for _doviz in _doviz_verisi:
+	
+	    _doviz_verileri = _doviz.find_all("li")
+	
+	for _altin in _altin_verisi:
+	
+	    _altin_verileri = _altin.find_all("li")
+
+def coin(_b):
+    _coin_url = requests.get('https://api.binance.com/api/v3/ticker/price')
+    _coinler = json.loads(_coin_url.content)
+    _coin_fiyatlari = 0
+    _coin_adi = _b
+
+    for _coin in _coinler:
+        if _coin['symbol'] == _coin_adi:
+            _coin_fiyatlari = _coin['price']
+            a = (_coin['price'])
+            return a
+ 
+def dolar():
+    _dolar = []
+    _dolar.append((_doviz_verileri[6].text).strip())
+    _dolar.append((_doviz_verileri[8].text).strip())
+    _dolar.append((_doviz_verileri[9].text).strip())
+    _dolar.append(((_doviz_verileri[10].text).strip())[:(len(_doviz_verileri[10].text) - 1)])
+    return _dolar
+
+def euro():
+    _euro = []
+    _euro.append((_doviz_verileri[12].text).strip())
+    _euro.append((_doviz_verileri[14].text).strip())
+    _euro.append((_doviz_verileri[15].text).strip())
+    _euro.append(((_doviz_verileri[16].text).strip())[:(len(_doviz_verileri[16].text) - 1)])
+    return _euro
+
+def altin():
+    _altin = []
+    _altin.append((_altin_verileri[5].text).strip())
+    _altin.append((_altin_verileri[6].text).strip())
+    _altin.append((_altin_verileri[7].text).strip())
+    _altin.append(((_altin_verileri[8].text).strip())[:(len(_doviz_verileri[8].text) - 1)])
+    return _altin
