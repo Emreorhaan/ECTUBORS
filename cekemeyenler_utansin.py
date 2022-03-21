bdd133673acfbb272d1c2458a6a9d00e9a10260a
Hamza Demir <demha082@gmail.com>
2022-03-21 22:45:05 +0300

    akaryakıt ve petrol verileri eklendi

--- cekemeyenler_utansin.py
+++ cekemeyenler_utansin.py
@@ -6,11 +6,15 @@ from requests.api import request
 _doviz_url = "http://bigpara.hurriyet.com.tr/doviz/"
 _altin_url = "http://bigpara.hurriyet.com.tr/altin/"
 _hisse_url = "https://bigpara.hurriyet.com.tr/borsa/hisse-senetleri/"
+_petrol_url = "https://bigpara.hurriyet.com.tr/kobi/dunya-emtia-borsalari/brent-petrol/"
+_akaryakıt_url = "https://www.petrolofisi.com.tr/akaryakit-fiyatlari"
 
 def kurcek():
     global _doviz_verileri
     global _altin_verileri
     global _hisse_verileri
+    global _petrol_verileri
+    global _akaryakıt_verileri
 
     _r = requests.get(_doviz_url)
     _soup = BeautifulSoup(_r.content,"html.parser")
@@ -20,12 +24,22 @@ def kurcek():
     
     _r3 = requests.get(_hisse_url)
     _soup3 = BeautifulSoup(_r3.content,"html.parser")
+
+    _r4 = requests.get(_petrol_url)
+    _soup4 = BeautifulSoup(_r4.content,"html.parser")
+
+    _r5 = requests.get(_akaryakıt_url)
+    _soup5 = BeautifulSoup(_r5.content,"html.parser")
 	
     _doviz_verisi = _soup.find_all("div",{"class":"tableBox srbstPysDvz"})
 	
     _altin_verisi = _soup2.find_all("div",{"class":"table wide pgAltin"})
 
     _hisse_verisi = _soup3.find_all("div", {"class":"contentLeft"})
+
+    _petrol_verisi = _soup4.find_all("div",{"class":"tableCnt"})
+
+    _akaryakıt_verisi = _soup5.find_all("div",{"class":"current-price-result-container current-price-result-container--secondary"})
 
     for _hisse in _hisse_verisi:
         
@@ -38,6 +52,14 @@ def kurcek():
     for _altin in _altin_verisi:
         
         _altin_verileri = _altin.find_all("li")
+
+    for _petrol in _petrol_verisi:
+        
+        _petrol_verileri = _petrol.find_all("li")
+    
+    for _akaryakıt in _petrol:
+        
+        _akaryakıt_verileri = _akaryakıt.find_all("td")
 
 def coin(_b):
     _coin_url = requests.get('https://api.binance.com/api/v3/ticker/price')
@@ -80,3 +102,17 @@ def hisse():
     _hisse.append((_hisse_verileri[11].text).strip())
     _hisse.append((_hisse_verileri[12].text).strip())
     return _hisse
+
+def petrol():
+    _petrol = []
+    _petrol.append((_petrol_verileri[7].text).strip())
+    _petrol.append((_petrol_verileri[10].text).strip())
+    return _petrol
+
+def akaryakıt():
+    _akaryakıt = []
+    _akaryakıt.append((_akaryakıt_verileri[1].text).strip())
+    _akaryakıt.append((_akaryakıt_verileri[2].text).strip())
+    _akaryakıt.append((_akaryakıt_verileri[3].text).strip())
+    _akaryakıt.append((_akaryakıt_verileri[4].text).strip())
+    return _akaryakıt
